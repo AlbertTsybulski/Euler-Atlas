6e7406c4279577c8776ed1b5b4d64765de75be44
AlbertTsybulski <acodenyc@gmail.com>
2026-07-10 17:53:02 -0400

    Revert "Fix analytics and speed monitoring."
    This reverts commit c3d3e4e68b12972c9eaf78b7e04d50efa7d9463c.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import { Analytics } from "@vercel/analytics/vue"
-import { SpeedInsights } from "@vercel/speed-insights/vue"
 import 'mathlive'
 import 'katex/dist/katex.min.css'
 import App from './App.vue'
